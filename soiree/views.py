cb800af6b85c0564ba79db085b8f0261a24f6135
Kate Jones <kate.jones.h2o@gmail.com>
2019-05-19 17:50:56 -0600

    Added a view for individual questions

--- soiree/views.py
+++ soiree/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
+'''
 def index(request):
     return HttpResponse("Bienvenue a l'Index de Votes!")
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+'''
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('soiree/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
